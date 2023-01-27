3cc57dc32eb1d96d7d74644d6794e0ec09235618
sarahcompsci <123202075+sarahcompsci@users.noreply.github.com>
2023-01-27 14:57:09 -0500

    Create circlearea.cpp

---
+++ circlearea.cpp
@@ -0,0 +1,23 @@
+//This program will output the circumference and area 
+//of the circle with a given radius.
+
+//Sarah Bender
+
+#include <iostream>
+using namespace std;
+
+const double PI = 3.14;
+const double RADIUS = 5.4;
+
+int main()
+{
+  float area;           
+  int circumference;  
+  cout << "The circumference of the circle is " << 2 * PI * RADIUS << endl;
+  cout << "The area of the circle is " << PI * RADIUS * RADIUS << endl;
+}
+
+//Change the data of circumference from float to int. Run the program and record the results. Explain what happened to get the results.
+//The circumference of the circle is 33.912
+//The area of the circle is 91.5624
+//The result was most likely the same as before because the computer recognized that the output of the program could not be a whole integer in this case. 
