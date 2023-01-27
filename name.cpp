a53335bf19d854f2afc0e8bd728b63c644258257
sarahcompsci <123202075+sarahcompsci@users.noreply.github.com>
2023-01-27 14:37:42 -0500

    Create name.cpp

---
+++ name.cpp
@@ -0,0 +1,20 @@
+//This program will write the name, address, and telephone
+//number of the programmer.
+
+//Sarah Bender
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  cout << "************" << endl;
+  cout << "Programmer: Sarah Bender" << endl;
+  cout << "            19 Homestead Village Drive" << endl;
+  cout << "            Warwick, NY 10990" << endl;
+  cout << " " << endl;
+  cout << "Telephone: 845-545-7315" << endl;
+  cout << "************" << endl;
+
+  return 0;
+}
